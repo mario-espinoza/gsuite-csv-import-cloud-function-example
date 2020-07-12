9a649332ae6cf95751abcedeeed037ab275f1443
mespinozas <mario@polinom.io>
2020-07-12 19:47:36 -0400

    moved to master ref

--- scripts/05-create-cloud-function.sh
+++ scripts/05-create-cloud-function.sh
@@ -4,7 +4,7 @@ source .env
 
 gcloud alpha functions add-iam-policy-binding $CF_NAME --member=allUsers --role=roles/cloudfunctions.invoker
 gcloud functions deploy $CF_NAME \
-  --source https://source.developers.google.com/projects/$PROJECT_ID/repos/$REPOSITORY_ID/moveable-aliases/master/paths/$SOURCE \
+  --source https://source.developers.google.com/projects/$PROJECT_ID/repos/$REPOSITORY_ID \
   --runtime $CF_RUNTIME \
   --trigger-bucket=$BUCKET_NAME \
   --set-env-vars=$CF_ENV_VARS
