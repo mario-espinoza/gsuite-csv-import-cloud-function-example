46e26d7445021e6ff0dfe9830e9364db29bd3158
mespinozas <mario@polinom.io>
2020-07-12 20:26:47 -0400

    updated deploy

--- scripts/05-deploy-cloud-function.sh
+++ scripts/05-deploy-cloud-function.sh
@@ -3,9 +3,9 @@
 source .env
 
 gcloud functions deploy $CF_NAME --entry-point=$CF_NAME \
-  --source https://source.developers.google.com/projects/$PROJECT_ID/repos/$REPOSITORY_ID \
+  --source https://source.developers.google.com/projects/$PROJECT_ID/repos/$REPOSITORY_ID/index.js \
   --runtime $CF_RUNTIME \
   --trigger-bucket=$BUCKET_NAME \
-  --set-env-vars=$CF_ENV_VARS
+  --set-env-vars="$CF_ENV_VARS"
 
 gcloud alpha functions add-iam-policy-binding $CF_NAME --member=allUsers --role=roles/cloudfunctions.invoker
