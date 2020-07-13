734cd3a6c8f4b2aaab74b79ff0dc3246429b1ce2
mespinozas <mario@polinom.io>
2020-07-12 20:05:01 -0400

    updated create cf script

--- scripts/05-create-cloud-function.sh
+++ scripts/05-create-cloud-function.sh
@@ -2,9 +2,10 @@
 
 source .env
 
-gcloud alpha functions add-iam-policy-binding $CF_NAME --member=allUsers --role=roles/cloudfunctions.invoker
-gcloud functions deploy $CF_NAME \
+gcloud functions deploy $CF_NAME --entry-point=$CF_NAME \
   --source https://source.developers.google.com/projects/$PROJECT_ID/repos/$REPOSITORY_ID \
   --runtime $CF_RUNTIME \
   --trigger-bucket=$BUCKET_NAME \
   --set-env-vars=$CF_ENV_VARS
+
+gcloud alpha functions add-iam-policy-binding $CF_NAME --member=allUsers --role=roles/cloudfunctions.invoker
